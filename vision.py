e5c6100db5aae16a01a51427d545fc0d04509958
sushma <sushma@life9sys.com>
2024-02-01 16:42:05 +0530

    new

--- vision.py
+++ vision.py
@@ -6,31 +6,39 @@ import streamlit as st
 import os
 import google.generativeai as genai
 from PIL import Image
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 model = genai.GenerativeModel("gemini-pro-vision")
-def get_gemini_response(input, image):
-    if image!="":
-        response = model.generate_content([input, image])
+def get_gemini_response(input,image):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    if input!="":
+       response = model.generate_content([input,image])
     else:
-        response = model.generate_content(image)
+       response = model.generate_content(image)
     return response.text
 
-   
+##initialize our streamlit app
 
-st.set_page_config(page_title="gemini image demo")
-st.header("Gemini image application")
-input = st.text_input("Input prompt: ", key="input")
-uploaded_file = st.file_uploader("choose an image...", type=["jpg","jpeg","png"])
-image = ""
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="uploaded image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-submit =st.button("tell me about the image")
 
-if submit :
-    response = get_gemini_response(input, image)
-    st.subheader("the response is")
+submit=st.button("Tell me about the image")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input,image)
+    st.subheader("The Response is")
     st.write(response)
